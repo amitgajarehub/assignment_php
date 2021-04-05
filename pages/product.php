14ea986e953a73a74b39946247fdf0775f4d05a8
amitgajarehub <amitgajare2018@gmail.com>
2021-04-05 19:56:09 +0530

    validate products fields

--- pages/product.php
+++ pages/product.php
@@ -14,7 +14,7 @@
 	    </div>
 		
 		<!-- product -->
-		<form action="../code/function.php" method="post" accept-charset="utf-8" enctype="multipart/form-data">
+		<form action="../code/function.php" method="post" accept-charset="utf-8" enctype="multipart/form-data" onsubmit="return validateForm()">
 			<div class="section content_section">
 				<div class="container">
 					<div class="filable_form_container">
@@ -23,13 +23,15 @@
 								<li class="fileds">
 									<div class="name_fileds">
 										<label>Product Name</label>
-										<input name="name" type="text" required=""> 
+										<input name="name" type="text" onchange="validateName()"> 
+										<div class="error" id="nameErr"></div>
 									</div>
 								</li>
 								<li class="fileds">
 									<div class="name_fileds">
 										<label>Product Price</label>
-										<input name="price" type="text" required=""> 
+										<input name="price" type="text" onchange="validatePrice()">
+										<div class="error" id="priceErr"></div> 
 									</div>
 								</li>
 								<li class="fileds">
@@ -41,16 +43,17 @@
 								<li class="fileds">
 									<div class="name_fileds">
 										<label>Select Category</label>
-										<select name="category" class="select category" style="z-index: 10; opacity: 0;" required>
+										<select name="category" class="select category" style="z-index: 10; opacity: 0;">
+											<option value=""></option>
 											<option value="mobile">Mobile</option>
 											<option value="automobile">Automobile</option>
-										</select><span class="select">Mobile</span> 
+										</select><span class="select"></span> 
 									</div>
 								</li>
 							</ul>
 							<div class="next_btn_block">
 								<div class="next_btn">
-									<button type="submit" name="product_submit">Submit  
+									<button type="submit" name="product_submit" id="product_submit">Submit  
 										<span><img src="../web/images/small_triangle.png" alt="small_triangle"> </span>
 									</button>
 								</div>
--- web/js/custom_script.js
+++ web/js/custom_script.js
@@ -1,3 +1,4 @@
+//product form validation
 
 	//image type validation
 	function validateImage() {
@@ -12,3 +13,67 @@
 	    }
 	    return true;
 	}
+
+	// Defining a function to display error message
+	function printError(elemId, hintMsg) {
+	  $("#" + elemId).html(hintMsg);
+	}
+		
+	// Product name validate
+	function validateName () {
+		
+		var name = $('[name="name"]').val();
+
+		var regex = /^\w+$/;                
+        if(regex.test(name) === false) {
+            printError("nameErr", "Please enter a valid Name");
+            $("#nameErr").addClass("mystyle");
+        } else {
+            printError("nameErr", "");
+            $("#nameErr").removeClass("mystyle");
+            nameErr = false;
+        }
+	}
+
+	// Product price validate
+	function validatePrice () {
+		
+		var price = $('[name="price"]').val();
+
+		var regex = /^\d+(\.\d{1,2})?$/;                
+        if(regex.test(price) === false) {
+            printError("priceErr", "Price - Compulsory & Decimal ");
+            $("#priceErr").addClass("mystyle");
+        } else {
+            printError("priceErr", "");
+            $("#priceErr").removeClass("mystyle");
+            priceErr = false;
+        }
+	}
+
+	//Not fill any field
+	function validateForm() {
+		
+		//get values
+		var name = $('[name="name"]').val();
+		var price = $('[name="price"]').val();
+		var category = $('[name="category"]').val();
+
+		//check values
+		if (name == "") {
+			alert("Product Name - Compulsory & Alphanumeric");
+			return false;
+		}
+
+		if (price == "") {
+			alert("Price, Compulsory & Decimal ");
+			return false;
+		}
+
+		if (category == "") {
+			alert("Product Category - Compulsory");
+			return false;
+		}
+	}
+
+/*************** Script End ****************/
